0e162772ac85592851c86ad3bcfeddef1a9d7950
Ahmed Amin <ahmedamin372002@gmail.com>
2026-03-21 14:30:35 +0200

    chore: refine Jest configuration and verify setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,11 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
